2d39e62148b26fc0d443a253b099e3813f0052a8
Christopher Dickson <chrisd1100@gmail.com>
2017-06-27 21:48:14 -0400

    more notes

--- include/uncurl/uncurl.h
+++ include/uncurl/uncurl.h
@@ -26,17 +26,18 @@ UNCURL_EXPORT int32_t uncurl_connect(struct uncurl *uc, struct uncurl_conn **ucc
 //XXX uncurl_send_req_body(struct uncurl_conn *ucc, char *body, uint32_t body_len);
 UNCURL_EXPORT int32_t uncurl_read_response_header(struct uncurl_conn *ucc);
 UNCURL_EXPORT int32_t uncurl_get_status_code(struct uncurl_conn *ucc, int32_t *status_code);
-//XXX uncurl_get_res_header_field_int(struct uncurl_conn *ucc, char *name, int32_t *value);
-//XXX uncurl_get_res_header_field_str(struct uncurl_conn *ucc, char *name, char **value);
-//XXX uncurl_read_res_body(struct uncurl_conn *ucc, char *body, uint32_t body_len);
+//XXX uncurl_read_res_body(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint32_t *bytes_read);
+//XXX uncurl_read_res_body_all(struct uncurl_conn *ucc, char **body, uint32_t *body_len);
 UNCURL_EXPORT void uncurl_close(struct uncurl_conn *ucc);
 UNCURL_EXPORT void uncurl_destroy(struct uncurl *uc);
 
-UNCURL_EXPORT void uncurl_set_request_header(struct uncurl_conn *ucc, ...);
+//XXX uncurl_parse_url(char *url, struct uncurl_info *info);
 //XXX uncurl_clear_req_header(struct uncurl_conn *ucc);
+//XXX uncurl_get_res_header_field_int(struct uncurl_conn *ucc, char *name, int32_t *value);
+//XXX uncurl_get_res_header_field_str(struct uncurl_conn *ucc, char *name, char **value);
+
+UNCURL_EXPORT void uncurl_set_request_header(struct uncurl_conn *ucc, ...);
 UNCURL_EXPORT int32_t uncurl_send_request(struct uncurl_conn *ucc, char *method, char *path, char *body, uint32_t body_len);
-//XXX uncurl_read_res_body_chunked(struct uncurl conn *ucc, char *body, uint32_t body_len);
-//XXX uncurl_get_res_body_type(int32_t type, int32_t *content_length);
 UNCURL_EXPORT int32_t uncurl_read_response_body(struct uncurl_conn *ucc, char **body, uint32_t *body_len);
 UNCURL_EXPORT int32_t uncurl_parse_url(char *url, int32_t *scheme, char **host, uint16_t *port, char **path);
 
