6a2f1f93eb58fc7c1c29e22d47c9f8db362b586c
Christopher Dickson <chrisd1100@gmail.com>
2017-11-23 08:24:37 -0500

    tls cleanup for accept, websocket cleanup, origin checking, bind to specific address on listen

--- include/uncurl/uncurl.h
+++ include/uncurl/uncurl.h
@@ -32,15 +32,18 @@ UNCURL_EXPORT void uncurl_free_tls_ctx(struct uncurl_tls_ctx *uc_tls);
 UNCURL_EXPORT int32_t uncurl_new_tls_ctx(struct uncurl_tls_ctx **uc_tls_in);
 UNCURL_EXPORT int32_t uncurl_set_cacert(struct uncurl_tls_ctx *uc_tls, char *cacert, size_t size);
 UNCURL_EXPORT int32_t uncurl_set_cacert_file(struct uncurl_tls_ctx *uc_tls, char *cacert_file);
+UNCURL_EXPORT int32_t uncurl_set_cert_and_key_file(struct uncurl_tls_ctx *uc_tls, char *cert_file, char *key_file);
 
 /*** CONNECTION ***/
 UNCURL_EXPORT struct uncurl_conn *uncurl_new_conn(struct uncurl_conn *parent);
 UNCURL_EXPORT int32_t uncurl_connect(struct uncurl_tls_ctx *uc_tls, struct uncurl_conn *ucc,
 	int32_t scheme, char *host, uint16_t port);
-UNCURL_EXPORT int32_t uncurl_listen(struct uncurl_conn *ucc, uint16_t port);
+UNCURL_EXPORT int32_t uncurl_listen(struct uncurl_conn *ucc, char *bind_ip4, uint16_t port);
 UNCURL_EXPORT int32_t uncurl_accept(struct uncurl_tls_ctx *uc_tls, struct uncurl_conn *ucc,
 	struct uncurl_conn **ucc_new_in, int32_t scheme);
 UNCURL_EXPORT void uncurl_close(struct uncurl_conn *ucc);
+UNCURL_EXPORT int32_t uncurl_poll(struct uncurl_conn *ucc, int32_t timeout_ms);
+UNCURL_EXPORT void uncurl_get_socket(struct uncurl_conn *ucc, void *socket);
 UNCURL_EXPORT void uncurl_set_option(struct uncurl_conn *ucc, int32_t opt, int32_t val);
 
 /*** REQUEST ***/
@@ -61,13 +64,12 @@ UNCURL_EXPORT int32_t uncurl_get_header(struct uncurl_conn *ucc, char *key, int3
 #define uncurl_get_header_str(ucc, key, val_str) uncurl_get_header(ucc, key, NULL, val_str)
 
 /*** WEBSOCKETS ***/
+UNCURL_EXPORT void uncurl_ws_allow_origin(struct uncurl_conn *ucc, char *origin);
 UNCURL_EXPORT int32_t uncurl_ws_connect(struct uncurl_conn *ucc, char *path, char *origin);
 UNCURL_EXPORT int32_t uncurl_ws_accept(struct uncurl_conn *ucc);
-UNCURL_EXPORT int32_t uncurl_ws_write(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, int32_t opcode);
-UNCURL_EXPORT int32_t uncurl_ws_poll(struct uncurl_conn *ucc, int32_t timeout_ms);
-UNCURL_EXPORT void uncurl_ws_get_socket(struct uncurl_conn *ucc, void *socket);
+UNCURL_EXPORT int32_t uncurl_ws_write(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint8_t opcode);
 UNCURL_EXPORT int32_t uncurl_ws_read(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint8_t *opcode);
-#define uncurl_ws_close(ucc) uncurl_ws_write(ucc, NULL, 0, UNCURL_WSOP_CLOSE)
+UNCURL_EXPORT int32_t uncurl_ws_close(struct uncurl_conn *ucc, uint16_t status_code);
 
 /*** HELPERS ***/
 UNCURL_EXPORT int32_t uncurl_parse_url(char *url, struct uncurl_info *uci);
