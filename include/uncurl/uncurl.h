2ce418fc1fbbef46aac64414ca616f29f738352a
Christopher Dickson <chrisd1100@gmail.com>
2018-11-24 11:40:43 -0500

    settings passed as arguments, connect cleanup

--- include/uncurl/uncurl.h
+++ include/uncurl/uncurl.h
@@ -37,16 +37,15 @@ UNCURL_EXPORT int32_t uncurl_set_cert_and_key(struct uncurl_tls_ctx *uc_tls,
 	char *cert, size_t cert_size, char *key, size_t key_size);
 
 /*** CONNECTION ***/
-UNCURL_EXPORT struct uncurl_conn *uncurl_new_conn(struct uncurl_conn *parent);
+UNCURL_EXPORT struct uncurl_conn *uncurl_new_conn(void);
 UNCURL_EXPORT int32_t uncurl_connect(struct uncurl_tls_ctx *uc_tls, struct uncurl_conn *ucc,
-	int32_t scheme, char *host, uint16_t port);
+	int32_t scheme, char *host, uint16_t port, bool verify_host, int32_t timeout_ms);
 UNCURL_EXPORT int32_t uncurl_listen(struct uncurl_conn *ucc, char *bind_ip4, uint16_t port);
 UNCURL_EXPORT int32_t uncurl_accept(struct uncurl_tls_ctx *uc_tls, struct uncurl_conn *ucc,
-	struct uncurl_conn **ucc_new_in, int32_t scheme);
+	struct uncurl_conn **ucc_new_in, int32_t scheme, int32_t timeout_ms);
 UNCURL_EXPORT void uncurl_close(struct uncurl_conn *ucc);
 UNCURL_EXPORT int32_t uncurl_poll(struct uncurl_conn *ucc, int32_t timeout_ms);
 UNCURL_EXPORT void uncurl_get_socket(struct uncurl_conn *ucc, void *socket);
-UNCURL_EXPORT void uncurl_set_option(struct uncurl_conn *ucc, int32_t opt, int32_t val);
 
 /*** REQUEST ***/
 UNCURL_EXPORT void uncurl_set_header_str(struct uncurl_conn *ucc, char *name, char *value);
@@ -56,8 +55,9 @@ UNCURL_EXPORT int32_t uncurl_write_header(struct uncurl_conn *ucc, char *str0, c
 UNCURL_EXPORT int32_t uncurl_write_body(struct uncurl_conn *ucc, char *body, uint32_t body_len);
 
 /*** RESPONSE ***/
-UNCURL_EXPORT int32_t uncurl_read_header(struct uncurl_conn *ucc);
-UNCURL_EXPORT int32_t uncurl_read_body_all(struct uncurl_conn *ucc, char **body, uint32_t *body_len);
+UNCURL_EXPORT int32_t uncurl_read_header(struct uncurl_conn *ucc, int32_t timeout_ms);
+UNCURL_EXPORT int32_t uncurl_read_body_all(struct uncurl_conn *ucc, char **body, uint32_t *body_len,
+	int32_t timeout_ms, size_t max_body);
 //XXX uncurl_read_body(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint32_t *bytes_read);
 UNCURL_EXPORT int32_t uncurl_get_status_code(struct uncurl_conn *ucc, int32_t *status_code);
 UNCURL_EXPORT int8_t uncurl_check_header(struct uncurl_conn *ucc, char *name, char *subval);
@@ -66,10 +66,11 @@ UNCURL_EXPORT int32_t uncurl_get_header(struct uncurl_conn *ucc, char *key, int3
 #define uncurl_get_header_str(ucc, key, val_str) uncurl_get_header(ucc, key, NULL, val_str)
 
 /*** WEBSOCKETS ***/
-UNCURL_EXPORT int32_t uncurl_ws_connect(struct uncurl_conn *ucc, char *path, char *origin);
-UNCURL_EXPORT int32_t uncurl_ws_accept(struct uncurl_conn *ucc, char **origins, int32_t n_origins, bool secure);
+UNCURL_EXPORT int32_t uncurl_ws_connect(struct uncurl_conn *ucc, char *path, char *origin, int32_t timeout_ms);
+UNCURL_EXPORT int32_t uncurl_ws_accept(struct uncurl_conn *ucc, char **origins,
+	int32_t n_origins, bool secure, int32_t timeout_ms);
 UNCURL_EXPORT int32_t uncurl_ws_write(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint8_t opcode);
-UNCURL_EXPORT int32_t uncurl_ws_read(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint8_t *opcode);
+UNCURL_EXPORT int32_t uncurl_ws_read(struct uncurl_conn *ucc, char *buf, uint32_t buf_len, uint8_t *opcode, int32_t timeout_ms);
 UNCURL_EXPORT int32_t uncurl_ws_close(struct uncurl_conn *ucc, uint16_t status_code);
 
 /*** HELPERS ***/
--- src/net.h
+++ src/net.h
@@ -4,40 +4,26 @@
 #include <stdint.h>
 
 enum net_events {
-	NET_POLLIN = 0,
+	NET_POLLIN  = 0,
 	NET_POLLOUT = 1,
-};
-
-struct net_opts {
-	int32_t read_timeout;
-	int32_t connect_timeout;
-	int32_t accept_timeout;
-	int32_t read_buf;
-	int32_t write_buf;
-	int32_t keepalive;
-	int32_t tcp_nodelay;
-	int32_t reuseaddr;
 };
 
 struct net_context;
 
-int32_t net_error();
-int32_t net_would_block();
-int32_t net_in_progress();
-int32_t net_bad_fd();
+int32_t net_error(void);
+int32_t net_would_block(void);
+int32_t net_in_progress(void);
+int32_t net_bad_fd(void);
 
 void net_close(struct net_context *nc);
-void net_default_opts(struct net_opts *opts);
-void net_set_otf_opts(struct net_context *nc, struct net_opts *opts);
 int32_t net_poll(struct net_context *nc, int32_t net_event, int32_t timeout_ms);
 int32_t net_getip4(char *host, char *ip4, uint32_t ip4_len);
-int32_t net_connect(struct net_context **nc_in, char *ip4, uint16_t port, struct net_opts *opts);
-int32_t net_listen(struct net_context **nc_in, char *bind_ip4, uint16_t port, struct net_opts *opts);
-int32_t net_accept(struct net_context *nc, struct net_context **nc_in);
+int32_t net_connect(struct net_context **nc_out, char *ip4, uint16_t port, int32_t timeout_ms);
+int32_t net_listen(struct net_context **nc_out, char *bind_ip4, uint16_t port);
+int32_t net_accept(struct net_context *nc, struct net_context **child, int32_t timeout_ms);
 void net_get_socket(struct net_context *nc, void *socket);
-void net_get_opts(struct net_context *nc, struct net_opts *opts);
 
-int32_t net_write(void *ctx, char *buf, uint32_t buf_size);
-int32_t net_read(void *nc, char *buf, uint32_t buf_size);
+int32_t net_write(void *ctx, char *buf, size_t size);
+int32_t net_read(void *ctx, char *buf, size_t size, int32_t timeout_ms);
 
 #endif
