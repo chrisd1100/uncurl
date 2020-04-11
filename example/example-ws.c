763974d06041b9b2f35b3de627f43d67b6d8cf62
Christopher Dickson <chrisd1100@gmail.com>
2020-04-11 11:16:44 -0400

    updated examples

--- example/example-ws.c
+++ example/example-ws.c
@@ -8,18 +8,17 @@
 	#include <winsock2.h>
 #endif
 
-static void client()
+static void client(void)
 {
 	struct uncurl_tls_ctx *uc_tls = NULL;
 	uncurl_new_tls_ctx(&uc_tls);
 
-	struct uncurl_conn *ucc = uncurl_new_conn(NULL);
-	uncurl_connect(uc_tls, ucc, UNCURL_WS, "54.242.16.17", 5250);
+	struct uncurl_conn *ucc = uncurl_new_conn();
+	uncurl_connect(uc_tls, ucc, UNCURL_WS, "54.242.16.17", 5250, true, NULL, 0, 10000);
 
-
-	/*
+	int32_t status = -1;
 	uncurl_set_header_str(ucc, "User-Agent", "uncurl/0.0");
-	uncurl_ws_connect(ucc, "/", "https://cdd.com");
+	uncurl_ws_connect(ucc, "/", "https://cdd.com", 10000, &status);
 
 	int32_t buf_len = 1024;
 	char *buf = calloc(1, buf_len);
@@ -27,19 +26,18 @@ static void client()
 	while (1) {
 		uint8_t opcode = 0;
 		uncurl_ws_write(ucc, "chris", 5, UNCURL_WSOP_TEXT);
-		uncurl_ws_read(ucc, buf, buf_len, &opcode);
+		uncurl_ws_read(ucc, buf, buf_len, &opcode, 10000);
 		printf("%u %s\n", opcode, buf);
 		if (opcode != 1) break;
 	}
 
-	uncurl_ws_close(ucc);
-	*/
+	uncurl_ws_close(ucc, 1000);
 }
 
-static void server()
+static void server(void)
 {
-	struct uncurl_conn *ucc = uncurl_new_conn(NULL);
-	uncurl_listen(ucc, 5250);
+	struct uncurl_conn *ucc = uncurl_new_conn();
+	uncurl_listen(ucc, "127.0.0.1", 5250);
 
 	printf("listen\n");
 
@@ -47,24 +45,24 @@ static void server()
 		int32_t e;
 
 		struct uncurl_conn *client = NULL;
-		e = uncurl_accept(NULL, ucc, &client, UNCURL_WS);
+		e = uncurl_accept(NULL, ucc, &client, UNCURL_WS, 10000);
 		if (e == UNCURL_OK) {
 			printf("accept\n");
 
-			e = uncurl_ws_accept(client);
+			e = uncurl_ws_accept(client, NULL, 0, false, 10000);
 			printf("uncurl_ws_accept: %d\n", e);
 
 			char buf[128];
 			memset(buf, 0, 128);
 			uint8_t opcode = 0;
-			e = uncurl_ws_read(client, buf, 128, &opcode);
+			e = uncurl_ws_read(client, buf, 128, &opcode, 10000);
 			printf("uncurl_ws_read: %d\n", e);
 			printf("%u %s\n", opcode, buf);
 
 			char *msg = "Reply from C!";
 			uncurl_ws_write(client, msg, (uint32_t) strlen(msg), UNCURL_WSOP_TEXT);
 
-			uncurl_ws_close(client);
+			uncurl_ws_close(client, 1000);
 			uncurl_close(client);
 		}
 
--- example/example.c
+++ example/example.c
@@ -10,7 +10,8 @@
 	#include <winsock2.h>
 #endif
 
-int32_t uncurl_request(struct uncurl_tls_ctx *uc_tls, char *method, char *url, char *body)
+int32_t uncurl_request(struct uncurl_tls_ctx *uc_tls, char *method, char *url, char *body,
+	char *proxy_host, uint16_t proxy_port)
 {
 	int32_t r = UNCURL_ERR_DEFAULT;
 	int32_t e;
@@ -25,32 +26,30 @@ int32_t uncurl_request(struct uncurl_tls_ctx *uc_tls, char *method, char *url, c
 		//create connection object
 		ucc = uncurl_new_conn();
 
-		//set options here
-		uncurl_set_option(ucc, UNCURL_NOPT_CONNECT_TIMEOUT, 10000);
-
 		//make the socket/TLS connection
-		e = uncurl_connect(uc_tls, ucc, uci.scheme, uci.host, uci.port);
+		e = uncurl_connect(uc_tls, ucc, uci.scheme, uci.host, uci.port, true,
+			proxy_host, proxy_port, 10000);
 		if (e != UNCURL_OK) {r = e; goto uncurl_get_end;}
 
 		//set request headers
 		uncurl_set_header_str(ucc, "User-Agent", "uncurl/0.0");
-		if (body) {
+		if (body && body[0]) {
 			uncurl_set_header_str(ucc, "Content-Type", "application/json");
 			uncurl_set_header_int(ucc, "Content-Length", (uint32_t) strlen(body));
 		}
 
 		//write the request header and body
-		e = uncurl_write_header(ucc, method, uci.path);
+		e = uncurl_write_header(ucc, method, uci.path, UNCURL_REQUEST);
 		if (e != UNCURL_OK) {r = e; goto uncurl_get_end;}
 
 		//write body
-		if (body) {
+		if (body && body[0]) {
 			e = uncurl_write_body(ucc, body, (uint32_t) strlen(body));
 			if (e != UNCURL_OK) {r = e; goto uncurl_get_end;}
 		}
 
 		//read the response header
-		e = uncurl_read_header(ucc);
+		e = uncurl_read_header(ucc, 10000);
 		if (e != UNCURL_OK) {r = e; goto uncurl_get_end;}
 
 		//get the status code
@@ -61,7 +60,7 @@ int32_t uncurl_request(struct uncurl_tls_ctx *uc_tls, char *method, char *url, c
 		//read the response body
 		char *response = NULL;
 		uint32_t response_len = 0;
-		e = uncurl_read_body_all(ucc, &response, &response_len);
+		e = uncurl_read_body_all(ucc, &response, &response_len, 10000, 1024 * 1024 * 10);
 		if (e != UNCURL_OK) {r = e; goto uncurl_get_end;}
 
 		fprintf(stderr, "%s : %d\n\n", url, status_code);
@@ -86,7 +85,7 @@ int32_t main(int32_t argc, char **argv)
 	int32_t e;
 
 	if (argc < 3) {
-		printf("Usage: uncurl [method] [url] [body]\n");
+		printf("Usage: uncurl method url [body] [proxy-host] [proxy-port]\n");
 		return 0;
 	}
 
@@ -100,12 +99,16 @@ int32_t main(int32_t argc, char **argv)
 	struct uncurl_tls_ctx *uc_tls = NULL;
 	e = uncurl_new_tls_ctx(&uc_tls);
 
-	//if making HTTPS requests, set root certs from buffer or file
-	uncurl_set_cacert(uc_tls, (char **) CACERT, sizeof(CACERT) / sizeof(const char *));
-	//uncurl_set_cacert_file(uc_tls, "../cacert/cacert.pem");
-
 	if (e == UNCURL_OK) {
-		e = uncurl_request(uc_tls, argv[1], argv[2], (argc > 3) ? argv[3] : NULL);
+		//root CA certs
+		uncurl_set_cacert(uc_tls, (char *) CACERT, sizeof(CACERT));
+
+		uint16_t proxy_port = 0;
+		if (argc > 5)
+			proxy_port = (uint16_t) atoi(argv[5]);
+
+		e = uncurl_request(uc_tls, argv[1], argv[2], (argc > 3) ? argv[3] : NULL,
+			argc > 4 ? argv[4] : NULL, proxy_port);
 		if (e != UNCURL_OK) printf("uncurl_get error: %d\n", e);
 
 		uncurl_free_tls_ctx(uc_tls);
